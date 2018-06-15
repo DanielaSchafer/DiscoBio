93fd2c8f491c6f32ed293b3ebdda7304312b4895
Daniela Schafer <31318441+DanielaSchafer@users.noreply.github.com>
2018-06-14 21:35:46 -0400

    turned into function and prints distances

--- Project/MinMoleculeBox.py
+++ Project/MinMoleculeBox.py
@@ -3,45 +3,53 @@ from rdkit import Chem
 import sys
 #from rdkit import Point3D
 
-inputPath = 'C:\\Users\\Daniela\\Documents\\discoBio\\FOLDer\\testData.csv'
+dataPath = 'C:\\Users\\Daniela\\Documents\\discoBio\\FOLDer\\testData.csv'
 
-m = Chem.MolFromSmiles('C1OC1')
+def getDimentions(inputPath):
 
-minX = float('inf')
-maxX =  float('-inf')
-minY =  float('inf')
-maxY =  float('-inf')
-minZ =  float('inf')
-maxZ =  float('-inf')
+    minX = float('inf')
+    maxX =  float('-inf')
+    minY =  float('inf')
+    maxY =  float('-inf')
+    minZ =  float('inf')
+    maxZ =  float('-inf')
 
-f = open(inputPath,'r')
-arr = f.readlines()
-f.seek(0)
-length = len(arr)
-print(length)
+    f = open(inputPath,'r')
+    arr = f.readlines()
+    f.seek(0)
+    length = len(arr)
+    print(length)
 
-counter = 0
+    counter = 0
 
-for molecules in f:
-    if counter !=0:
-        cols = molecules.split(',')
-        print(cols[0])
-        suppl = Chem.SDMolSupplier(cols[0])
-        for molecule in suppl:
-            m = Chem.MolFromSmiles(molecule)
-            for atom in m.GetAtoms():
-                point = atom.GetAtomPosition()
-                if point.x <minX:
-                    minX = point.x
-                elif point.x >maxX:
-                    maxX = point.x
-                if point.y <minY:
-                    minY = point.y
-                elif point.y >maxY:
-                    maxY = point.y
-                if point.x <minX:
-                    minX = point.x
-                elif point.z >maxZ:
-                    maxZ = point.z
-    counter=counter+1
-print(minX,maxX,minY,maxY)
+    for molecules in f:
+        if counter !=0:
+            cols = molecules.split(',')
+            print(cols[0])
+            suppl = Chem.SDMolSupplier(cols[0])
+            for molecule in suppl:
+                m = Chem.MolFromSmiles(molecule)
+                for atom in m.GetAtoms():
+                    point = atom.GetAtomPosition()
+                    if point.x <minX:
+                        minX = point.x
+                    elif point.x >maxX:
+                        maxX = point.x
+                    if point.y <minY:
+                        minY = point.y
+                    elif point.y >maxY:
+                        maxY = point.y
+                    if point.x <minX:
+                        minX = point.x
+                    elif point.z >maxZ:
+                        maxZ = point.z
+        counter=counter+1
+
+    x = maxX-minX
+    y = maxY-minY
+    z = maxZ-minZ
+
+    print(x,y,z)
+
+
+getDimentions(dataPath)
