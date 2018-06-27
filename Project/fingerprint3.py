6d2d187cba44b445b132d3d6c9ea00ebf0393ce5
Daniela Schafer <schaferd@winchesterthurston.org>
2018-06-26 21:37:50 -0400

    steps towards multiprocessing

--- Project/fingerprint3.py
+++ Project/fingerprint3.py
@@ -5,6 +5,8 @@ import os
 import sys
 import re
 import time
+import multiprocessing 
+from multiprocessing import Pool
 
 now = datetime.datetime.now()
 
@@ -67,32 +69,54 @@ def compareFolds(fingerprintHM, fold1, fold2):
     fold2List = readFoldFile(fold2)
     ms = ""
     counter = 0
-    valuesAccountedFor = 0
+    #valuesAccountedFor = 0
+    extremeVals = [float('inf'),float('-inf'),0,0]
+
+
     for line in fold1List:
-        print(weakestLinkVal,strongestLinkVal, str((counter/len(fold1List))*100))
+        print(str(int((counter/len(fold1List))*100)))
         counter = counter+1
 
         cols = line.split(" ")
         ms = cols[3]
         if ms in fingerprintHM:
-            for line2 in fold2List:
+            extremeVals = compareToSecondFold(fingerprintHM,ms,fold2List,extremeVals)
+                    
+    output = "For partitions: "+ fold1 + " and "+ fold2+"\nAverage Similaraity: "+str(float(extremeVals[3])/float(extremeVals[2]))+"\nstrongest link: " + str(extremeVals[1]) +"\nweakest link: "+str(extremeVals[0])
+    print(output)
+    return output
+
+def compareToSecondFold(fingerprintHM, ms,fold2List,extremeVals):
+    for line2 in fold2List:
                 cols2 = line2.split(" ")
                 ms2 = cols2[3]
                 if ms2 in fingerprintHM:
                     sim = fingerprintHM[ms][0] | fingerprintHM[ms2][0]
-                    simTotal = simTotal +sim
-                    if sim < weakestLinkVal:
-                        weakestLinkVal = sim
-                        weakestLink[0] = ms
-                        weakestLink[1] = ms2
-                    if sim > strongestLinkVal:
-                        strongestLinkVal = sim
-                        strongestLink[0] = ms
-                        strongestLink[1] = ms2
-                    valuesAccountedFor = valuesAccountedFor +1
-    output = "For partitions: "+ fold1 + " and "+ fold2+"\nAverage Similaraity: "+str(simTotal/valuesAccountedFor)+"\nstrongest link: " + str(strongestLinkVal) + " between "+str(strongestLink[0]) +" and " +str(strongestLink[1])+"\nweakest link: "+str(weakestLinkVal)+" between "+str(weakestLink[0])+" and "+str(weakestLink[1])
-    print(output)
-    return output
+                    #simTotal = simTotal +sim
+                    extremeVals[3] = extremeVals[3]+sim
+                    if sim < extremeVals[0]:
+                        #weakestLinkVal = sim
+                        extremeVals[0] = sim
+                        #weakestLink[0] = ms
+                        #extremeVals[0][1] = ms
+                        #weakestLink[1] = ms2
+                        #extremeVals[0][2] = ms2
+                    if sim > extremeVals[1]:
+                        #strongestLinkVal
+                        extremeVals[1] = sim
+                        #strongestLink
+                        #extremeVals[1][1] = ms
+                        #strongestLink
+                        #extremeVals[1][2] = ms2
+                    #valuesAccountedFor = valuesAccountedFor +1
+                    extremeVals[2] = extremeVals[2]+1
+    return(extremeVals)
+
+
+def processing(folds):
+    chunks = [lis[i::5] for i in range(5)]
+    pool = Pool(processes=5)
+    result= pool.map(compareToSecondFold,chunks)
 
 
 def getSimilaritiesBetweenFolds(foldArr,csvData,dataPath,foldPath,ouputPath):
