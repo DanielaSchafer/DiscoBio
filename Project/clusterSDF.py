7895e62f3afbcdba07e5e613983c0301f6f1410e
Daniela Schafer <schaferd@winchesterthurston.org>
2018-06-28 21:19:40 -0400

    new files

---
+++ Project/clusterSDF.py
@@ -0,0 +1,152 @@
+import pybel
+import openbabel
+import sys
+import os
+
+def findNeighbors(threshold,m,isAdded,fingerprintHM):
+    neigh = list()
+    for m2 in fingerprintHM:
+        if m2 not in isAdded:
+            sim = fingerprintHM[m][0] | fingerprintHM[m2][0]
+            if float(sim) > float(threshold):
+                neigh.append(m2)
+    return neigh
+
+def makeClusters(threshold,data, isAdded,fingerprintHM):
+    groups = list()
+    data.seek(0)
+
+    counter = 0
+
+    for m in data:
+        tempLine = m.rstrip("\n")
+        tempLine = tempLine.rstrip()
+        line  = tempLine
+
+        cols = line.split(", ")
+        cols[0].rstrip()
+        cols[1].rstrip()
+        cols[2].rstrip()
+        mol = cols[0]+"/"+cols[1]+" "+cols[2]
+        if mol not in isAdded and cols[2] != 'nan':
+            group = list()
+            group.append(mol)
+            groupID = len(groups)
+            newGroup = findAllInGroup(mol,groupID,threshold,group,isAdded,fingerprintHM)
+            groups.append(newGroup)
+            for g in newGroup:
+                isAdded[g] = groupID
+        counter = counter +1
+    return groups
+
+def getFingerprintHM(csvData,data,fileType):
+      fingerprints = dict()
+      mCounter = 0
+      
+      counter = 0
+
+      for line in data:
+
+          tempLine = line.rstrip("\n")
+          tempLine = tempLine.rstrip()
+          line = tempLine
+
+          cols = line.split(", ")
+          cols[0].rstrip()
+          cols[1].rstrip()
+          cols[2].rstrip()
+          if cols[2] != 'nan':
+              m = pybel.readfile(fileType,dataPath+cols[0]+"/"+cols[1]+"."+fileType)
+              for mol in m:
+                fps = list()
+                fp = mol.calcfp()
+                fps.append(fp)
+                fingerprints[cols[0]+"/"+cols[1]+" "+cols[2]] = fps
+        
+      mCounter = mCounter+1
+      counter = counter +1
+
+      return fingerprints
+
+
+def findAllInGroup(m,groupID,threshold,groupMems,isAdded,fingerprintHM):
+    neigh = findNeighbors(threshold,m,isAdded,fingerprintHM)
+    neigh2 = neigh
+    if len(neigh) == 0:
+        return neigh
+
+    for n in neigh:
+        isAdded[n] = groupID
+    for n in neigh2:
+        return neigh+findAllInGroup(n,groupID,threshold,groupMems,isAdded,fingerprintHM)
+
+def evenOutGroups(groups):
+    maxLen = 0
+    newGroups = groups
+
+    for g in groups:
+        if len(g)>maxLen:
+            maxLen = len(g)
+    
+    smallestSum = 0
+    while smallestSum < maxLen and len(newGroups)>3:
+        mIndex = getMin(newGroups)
+        m = newGroups[mIndex]
+        newGroups.pop(mIndex)
+        newM = getMin(newGroups)
+        newGroups[newM] = newGroups[newM]+m
+    return newGroups
+    
+
+
+def getMin(lis):
+    minLen = 0
+    for i, l in enumerate(lis):
+        if len(l) < len(lis[minLen]):
+            minLen = i
+    return minLen
+
+
+def runner(csvData,dataPath,threshold,fileType,path):
+    data = open(csvData,'r')
+    fps = getFingerprintHM(csvData,data,fileType)
+    isAdded = dict()
+    groups = makeClusters(threshold,data,isAdded,fps)
+    groups = evenOutGroups(groups)
+
+
+    path = path+'/folds'+str(len(groups))+"-t-"+threshold+"/"
+    directory = os.path.dirname(path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+
+    with open(path+"info.txt",'w+') as info:
+        info.writelines("threshold: "+str(threshold)+" ")
+        for i, g in enumerate(groups):
+            info.writelines("mol in fold "+str(i)+": "+str(len(g))+"\n")
+            for j,m in enumerate(g):
+                cols = m.split(" ")
+                groups[i][j] = str(1)+" "+str(cols[1])+" none "+str(cols[0])+".mol "
+
+        for fold in range(0,len(groups)):
+            newTrain = open(path+'train'+str(fold)+'.types','w+')
+            newTest = open(path+'test'+str(fold)+'.types','w+')
+            newTest.writelines("%s\n" % item for item in groups[fold])
+            for f in range(0,len(groups)):
+                if(f != fold):
+                    newTrain.writelines("%s\n" % item for item in groups[f])
+            newTrain.close()
+            newTest.close()
+
+        info.writelines("\ntotalFolds "+str(len(groups)))
+        print("folds "+str(len(groups)))
+
+csvData = sys.argv[1]
+dataPath = sys.argv[2]
+threshold = sys.argv[3]
+fileType = sys.argv[4]
+outputPath = sys.argv[5]
+
+runner(csvData,dataPath,threshold,fileType,outputPath)
+
