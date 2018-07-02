71b565852f9c0b7b676ac66f178ca3c409209f08
Daniela Schafer <schaferd@winchesterthurston.org>
2018-07-02 14:13:38 -0400

    added getDict method

--- Project/prediction1NN.py
+++ Project/prediction1NN.py
@@ -1,12 +1,25 @@
 import os
 import sys
 
-def findMostSimilar(key,data):
-
+def findMostSimilar(key,data,found):
+    mostSimilarVal = 0
+    mostSimilarKey = key
+    for k in data:
+        sim = 0
+        if set([key,k]) in found:
+            sim = found[set([key,k])] | data[key]
+        else:
+            sim = k | key
+            found[set([key,k]) = sim
+        if sim > mostSimilarVal:
+            mostSimilarVal = sim
+            mostSimilarKey = k
+    return mostSimilarKey
+            
 
 def getDict(csv):
     f = open(csv,'r')
-    data = dict()
+    fingerprints = dict()
     for line in f:
         cols = line.split(", ")
         
@@ -14,8 +27,12 @@ def getDict(csv):
         cols[1].rstrip()
         cols[2].rstrip()
         if cols[2] != 'nan':
-            data[cols[0]+"/"+cols[1]] = cols[2]
-
+            ms = pybel.readfile("mol",dataPath+cols[0]+"/"+cols[1]+".mol")
+            for m in ms:
+                fps = list()
+                fp = mol.calcfp()
+                fps.append(fp)
+                fingerprints[cols[0]+"/"+cols[1]] = fps[0]     
     return data
 
 
